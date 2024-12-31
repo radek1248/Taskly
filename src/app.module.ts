06bd2dbb51df138ab9aa97b0b6f5f4077da67f92
radek1248 <rbrunowski@wp.pl>
2024-12-31 11:53:48 +0100

    Connect db with the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { TaskModule } from './task/task.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    SequelizeModule.forRoot({
+      autoLoadModels: true,
+      synchronize: true,
+      sync: { alter: true },
+      dialect: 'mysql',
+      host: process.env.DB_HOST, // Database host
+      port: Number(process.env.DB_PORT), // Database port
+      username: process.env.DB_USERNAME, // Database username
+      password: process.env.DB_PASSWORD, // Database password
+      database: process.env.DB_DATABASE_NAME, // Database name
+    }),
+    TaskModule,
+    ConfigModule,
+  ],
   controllers: [],
   providers: [],
 })
