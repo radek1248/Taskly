7c8aa40cb705bcc63efbe687d8387497ab1c9165
radek1248 <rbrunowski@wp.pl>
2025-01-03 11:29:33 +0100

    Change tasks-collection methods' returned type

--- src/tasks-collection/tasks-collection.controller.ts
+++ src/tasks-collection/tasks-collection.controller.ts
@@ -13,30 +13,29 @@ import { CreateTaskDto } from '../task/dto/create-task.dto';
 import { TaskService } from '../task/task.service';
 import { Task } from '../task/task.entity';
 import { UpdateTaskDto } from '../task/dto/update-task.dto';
+import { TasksCollection } from './tasks-collection.entity';
 
 @Controller('tasks-collection')
 export class TasksCollectionController {
   constructor(
     private readonly tasksCollectionService: TasksCollectionService,
-    private readonly taskSerivce: TaskService,
+    private readonly taskService: TaskService,
   ) {}
 
   @Get()
-  async findAll(): Promise<CreateTasksCollectionDto[]> {
+  async findAll(): Promise<TasksCollection[]> {
     return await this.tasksCollectionService.findAll();
   }
 
   @Get(':collectionId')
-  async findOne(
-    @Param('collectionId') id: string,
-  ): Promise<CreateTasksCollectionDto> {
+  async findOne(@Param('collectionId') id: string): Promise<TasksCollection> {
     return await this.tasksCollectionService.findOne(id);
   }
 
   @Post()
   async createTasksCollection(
     @Body() taskCollectionDto: CreateTasksCollectionDto,
-  ): Promise<CreateTasksCollectionDto> {
+  ): Promise<TasksCollection> {
     return await this.tasksCollectionService.createTasksCollection(
       taskCollectionDto,
     );
@@ -46,7 +45,7 @@ export class TasksCollectionController {
   async updateTasksCollection(
     @Param('collectionId') id: string,
     @Body() taskCollectionDto: CreateTasksCollectionDto,
-  ): Promise<CreateTasksCollectionDto> {
+  ): Promise<any> {
     return await this.tasksCollectionService.updateTasksCollection(
       id,
       taskCollectionDto,
@@ -65,17 +64,17 @@ export class TasksCollectionController {
   async findAllTasks(
     @Param('collectionId') collectionId: string,
   ): Promise<Task[]> {
-    return await this.taskSerivce.findAll(collectionId);
+    return await this.taskService.findAll(collectionId);
   }
 
   @Get('/tasks/:taskId')
   async findOneTask(@Param('taskId') taskId: string): Promise<Task> {
-    return await this.taskSerivce.findOne(taskId);
+    return await this.taskService.findOne(taskId);
   }
 
   @Post('/tasks')
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-    return await this.taskSerivce.createTask(createTaskDto);
+    return await this.taskService.createTask(createTaskDto);
   }
 
   @Patch('/tasks/:taskId')
@@ -83,11 +82,11 @@ export class TasksCollectionController {
     @Param('taskId') taskId: string,
     @Body() updateTaskDto: UpdateTaskDto,
   ): Promise<Task> {
-    return await this.taskSerivce.updateTask(taskId, updateTaskDto);
+    return await this.taskService.updateTask(taskId, updateTaskDto);
   }
 
   @Delete('/tasks/:taskId')
   async deleteTask(@Param('taskId') taskId: string): Promise<void> {
-    await this.taskSerivce.deleteTask(taskId);
+    await this.taskService.deleteTask(taskId);
   }
 }
